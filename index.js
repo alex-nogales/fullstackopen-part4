6074aa80d2dcfd344be4e84cd8ac73c87edf1c22
Alex Nogales <a.nogales.cs@gmail.com>
2021-04-13 10:21:47 -0400

    Refactor controllers

--- index.js
+++ index.js
@@ -3,30 +3,14 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const Blog = require('./models/blog')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+
+app.use('/api/blogs', blogsRouter)
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
 
 // eslint-disable-next-line no-undef
 app.listen(config.PORT, () => {
