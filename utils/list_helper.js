86f18b885f2934c42310b6de9ac2ae57abdd01da
Alex Nogales <a.nogales.cs@gmail.com>
2021-04-14 11:08:02 -0400

    step1

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,4 +1,3 @@
-const _ = require('lodash')
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
     return 1
@@ -18,21 +17,6 @@ const favoriteBlog = (blogs) => {
     }
 }
 
-
-
-/* var services = ['weibo', 'facebook', 'twitter', 'xing'];
-var result = _.map(services, function(service){
-     var length = _.reject(array, function(el){
-           return (el.id.indexOf(service) < 0);
-     }).length;
-     return {id: service, count: length};
-}); */
-
-const mostBlogs = (blogs) => {
-    const authors = blogs.map(blog => blog.author).filter((value, index, self) => self.indexOf(value) === index)
-    return 0
-}
-
 module.exports = {
-    dummy, totalLikes, favoriteBlog, mostBlogs
+    dummy, totalLikes, favoriteBlog
 }
